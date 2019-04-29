d0b6fa43471174c39ef8c78ff361c46c5a6a83f5
Christopher W. Brown <christopherwilliambrown@gmail.com>
2019-04-29 14:50:10 -0400

    Begin logic for react management of rest services

--- ironclad-react/src/App.js
+++ ironclad-react/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import events from 'events'
+import ajax from './ajax'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nodes: []
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>Ironclad Headless </div>
+
+        <div> Create Nodes Component </div>
+
+
+        <div> List of Nodes Component </div>
+
+        <div>Basic Info: This app is a React based App that can can interact with the associated Drupal installation, if the user is logged in with appropriate user permissions. It uses axios and the
+RESTful Web Services, serialization  and a custom module to enable the data to move between applications without triggering a CORS error. </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
